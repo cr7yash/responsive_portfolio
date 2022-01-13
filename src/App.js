2b347669a91c6a31bfd012cda316dd6bd2a11fcf
Yash Srivastava <yashaqua@gmail.com>
2022-01-13 22:58:25 +0530

    add about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Introduction from "./Components/Introduction/Introduction";
+import About from "./Components/About/About";
 
 const App = () => {
   return (
     <div>
       <Introduction />
+      <About />
     </div>
   );
 };
